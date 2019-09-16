caa9d9066c47137cb257d9d922621394ebfe43f3
ranjithaavulka <55373259+ranjithaavulka@users.noreply.github.com>
2019-09-16 10:24:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,3 +13,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 console.log("pppppppppppppppppppppppppppppppppppppppp");//1st change
+console.log("ooooooooooooooooooooooo");//2nd change
