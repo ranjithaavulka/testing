29cb657cf32c1b8d3b69b26017b01a132180b30d
ranjithaavulka <ranjitha@cube.partners>
2019-09-16 10:09:29 +0530

    First test

---
+++ server.js
@@ -0,0 +1,14 @@
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+
+var mongoose       = require('mongoose');                     // mongoose for mongodb
+
+
+var db = require('./config/db');
+var port = process.env.PORT || 5555; 
+mongoose.connect(db.url); 
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
